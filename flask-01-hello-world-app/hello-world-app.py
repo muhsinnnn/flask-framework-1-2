f075f0b81146553ddc2e93d483da75309b884d50
muhsin <mysozuer@gmail.com>
2024-08-05 22:26:15 +0200

    second

--- flask-01-hello-world-app/hello-world-app.py
+++ flask-01-hello-world-app/hello-world-app.py
@@ -1,6 +1,21 @@
 from flask import Flask 
 app = Flask(__name__)
 
+@app.route("/")
+def head():
+    return "hello word clarusway-18"
+
+@app.route("/second")
+def head2():
+    return "second page"
+
+@app.route("/third")
+def head3():
+    return "buda 3 ncü sayfa"
+
+@app.route('/forth/<string:id>')
+def forth(id):
+    return f'Id of this page is {id}'
 
 if __name__ == '__main__':
 
